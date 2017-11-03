12a13cb11223b2211be3cc58b1a081740ad902f7
Ricardo <landinezrichard@gmail.com>
2017-11-02 21:49:49 -0500

    Añadido cambio color destacado al Personalizador del tema

--- functions/theme-customizer.php
+++ functions/theme-customizer.php
@@ -99,8 +99,101 @@
 			)
 		);
 
+	//---------------------------------
+	//SECCIÓN COLORES
+	//---------------------------------
+
+		//Color destacado
+		$wp_customize->add_setting('ryc_color_destacado', array(
+			'default' => '#50C5AB',
+			'sanitize_callback' => 'sanitize_hex_color'
+		));
+
+		$wp_customize->add_control( new WP_Customize_Color_Control(
+			$wp_customize,
+			'ryc_color_destacado', array(
+				'label' => __('Color destacado', 'ryc'),
+				'section' => 'colors',
+				'settings' => 'ryc_color_destacado'
+			)
+		));
+
 	}
 
 	add_action('customize_register', 'ryc_customizer');
+
+	function porcentaje($numero, $porcentaje){
+		return ( ($numero*$porcentaje)/100 );
+	}
+
+	//Oscurecer un color hexadecimal
+	function oscurecerColor($color, $cant){
+		$colores_rgb = array();
+
+		for($i = 0; $i < 3; $i++){
+			//extraer cada color (rojo, verde,azul)
+			$colores_rgb[$i] = substr($color,2*$i+1,2);
+
+			//convertir a enteros los string, que tengo en hexadecimal
+			$colores_rgb[$i] = hexdec($colores_rgb[$i]);
+
+			$intporcentaje = round( porcentaje($colores_rgb[$i], $cant) );
+
+			//verifico que no quede como negativo y resto
+			if($colores_rgb[$i]-$intporcentaje >= 0) $colores_rgb[$i] = $colores_rgb[$i]-$intporcentaje;
+
+			//convertir a hexadecimal
+			$colores_rgb[$i] = dechex( $colores_rgb[$i] );
+
+			//voy a validar que los string hexadecimales tengan dos caracteres
+			if( strlen( $colores_rgb[$i] )<2 ) $colores_rgb[$i] = "0".$colores_rgb[$i];
+		}
+
+		//voy a construir el color hexadecimal
+		$oscuridad = "#".$colores_rgb[0].$colores_rgb[1].$colores_rgb[2];
+		
+		//la función devuelve el valor del color hexadecimal resultante
+		return $oscuridad;
+	}
+
+
+
+	//Aplicando colores del selector de colores a los estilos
+	function ryc_customize_colors_css(){
+		$color_destacado = get_theme_mod('ryc_color_destacado');
+		?>
+			<style type="text/css">
+				h2,
+				a{
+					color: <?php echo $color_destacado; ?>;
+				}
+
+				.btn,
+				input[type="submit"],
+				input[type="button"]{
+					background: <?php echo $color_destacado; ?>;
+				}
+
+				.btn:hover{
+					background: <?php echo oscurecerColor($color_destacado, 15); ?>;
+				}
+
+				#main-menu ul li.featured a{
+					background: <?php echo $color_destacado; ?>;
+				}
+
+				#main-menu ul li.featured a:hover{
+					background: <?php echo oscurecerColor($color_destacado, 15); ?>;
+				}
+
+				input:focus,
+				textarea:focus{
+					border-color: <?php echo $color_destacado; ?>;
+				}
+			</style>
+		<?php
+	}
+
+	add_action('wp_head', 'ryc_customize_colors_css');
 
 ?>
